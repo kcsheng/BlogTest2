c770431d72efaf2c5d055616a97b7a16a721c47f
kcsheng <kacysheng@mail.com>
2020-06-22 13:04:43 +1000

    executed db:migrate and added comments for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :portfolios # most commonly used, scaffold-like, skinnier (NO prefilled items), database-setup
+  # minimalist type of architecture allowing for more room to customise
 end
