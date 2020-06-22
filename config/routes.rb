ec5cd275c31a3c8ae393a2c4a0735d58fb819d79
kcsheng <kacysheng@mail.com>
2020-06-22 13:06:29 +1000

    added comments for routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :portfolios # most commonly used, scaffold-like, skinnier (NO prefilled items), database-setup
+  resources :portfolios # most commonly used, scaffold-like, skinnier (NO prefilled items), with database
   # minimalist type of architecture allowing for more room to customise
 end
